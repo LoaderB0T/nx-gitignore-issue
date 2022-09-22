b56100df5c5ade34e93275d36c6c441b957f5e82
Janik Schumacher <rndmb0t@gmail.com>
2022-09-22 11:15:51 +0200

    initial commit

---
+++ apps/gitignore/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gitignore-issue-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'gitignore';
+}
